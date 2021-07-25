3786df2cbd1b1a2196497d28577d0396f02e3452
Enzo Arguello <xlsones003@gmail.com>
2021-07-25 12:10:40 -0300

    reduced unnecessary code from challenge 3, added new libs to tsc configuration

--- challenges/3-asynchronous-javascript/index.js
+++ challenges/3-asynchronous-javascript/index.js
@@ -4,35 +4,22 @@ var text3 = '3and 3typesetting 3industry.';
 var recorrerPalabras = function (text, msPerWord, callback) {
     if (msPerWord === void 0) { msPerWord = 1000; }
     var splitText = text.split(' ');
-    var i = 0;
-    var wordCount = 0;
-    splitText.forEach(function (el) {
-        if (i === splitText.length) {
+    var counter = 0;
+    var myInterval = setInterval(function () {
+        if (counter === splitText.length) {
+            clearInterval(myInterval);
+            console.log(counter + " palabras encontradas!");
             callback();
-            console.log(wordCount + " palabras encontradas!");
         }
         else {
-            setTimeout(function (el) { return console.log(el); }, msPerWord);
-            i++;
-            wordCount++;
+            console.log(splitText[counter]);
+            counter++;
         }
-    });
-    //const myInterval: number = setInterval((): void => {
-    //if (i === splitText.length) {
-    //clearInterval(myInterval);
-    //console.log(`${wordCount} palabras encontradas!`);
-    //return callback();
-    //} else {
-    //console.log(splitText[i]);
-    //i++;
-    //wordCount++;
-    //}
-    //}, msPerWord)
+    }, msPerWord);
 };
-//const recorrerPalabras = new Promise((resolve, reject) => {
-//});
 var myCallback = function () {
     console.log('proceso completo');
 };
-//recorrerPalabras(text1, 800, myCallback);
-recorrerPalabras(text1, 800, recorrerPalabras(text2, 800, recorrerPalabras(text3, 800, myCallback)));
+recorrerPalabras(text1, 800, myCallback);
+recorrerPalabras(text2, 800, myCallback);
+recorrerPalabras(text3, 800, myCallback);
--- challenges/3-asynchronous-javascript/index.ts
+++ challenges/3-asynchronous-javascript/index.ts
@@ -2,55 +2,28 @@ const text1: string = '1Lorem 1Ipsum 1is 1simply';
 const text2: string = '2dummy 2text 2of 2the 2printing';
 const text3: string = '3and 3typesetting 3industry.';
 
-const recorrerPalabras: Function = (text: string, msPerWord: number = 1000, callback: Function) => {
+const recorrerPalabras: Function = (text: string, msPerWord: number = 1000, callback: Function): void => {
 
   const splitText: string[] = text.split(' ');
-  let i: number = 0;
-  let wordCount: number = 0;
-  splitText.forEach((el: string) => {
-    if (i === splitText.length) {
+  let counter: number = 0;
+
+  const myInterval: number = setInterval((): void => {
+    if (counter === splitText.length) {
+      clearInterval(myInterval);
+      console.log(`${counter} palabras encontradas!`);
       callback();
-      console.log(`${wordCount} palabras encontradas!`);
     } else {
-      setTimeout((el: string) => console.log(el), msPerWord)
-      i++;
-      wordCount++;
+      console.log(splitText[counter]);
+      counter++;
     }
-  })
-  //const myInterval: number = setInterval((): void => {
-  //if (i === splitText.length) {
-  //clearInterval(myInterval);
-  //console.log(`${wordCount} palabras encontradas!`);
-  //return callback();
-  //} else {
-  //console.log(splitText[i]);
-  //i++;
-  //wordCount++;
-  //}
-  //}, msPerWord)
+  }, msPerWord)
 
 }
-
-//const recorrerPalabras = new Promise((resolve, reject) => {
-
-//});
 
 const myCallback: Function = (): void => {
   console.log('proceso completo');
 }
 
-//recorrerPalabras(text1, 800, myCallback);
-
-recorrerPalabras(
-  text1,
-  800,
-  recorrerPalabras(
-    text2,
-    800,
-    recorrerPalabras(
-      text3,
-      800,
-      myCallback
-    )
-  )
-);
+recorrerPalabras(text1, 800, myCallback,);
+recorrerPalabras(text2, 800, myCallback,);
+recorrerPalabras(text3, 800, myCallback,);
