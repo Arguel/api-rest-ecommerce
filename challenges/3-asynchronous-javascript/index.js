0ea8acb00d128cb309661313d996eba570e808fd
Enzo Arguello <xlsones003@gmail.com>
2021-07-30 14:48:13 -0300

    updated challenge 3 with async / await

--- challenges/3-asynchronous-javascript/index.js
+++ challenges/3-asynchronous-javascript/index.js
@@ -1,25 +1,31 @@
-var text1 = '1Lorem 1Ipsum 1is 1simply';
-var text2 = '2dummy 2text 2of 2the 2printing';
-var text3 = '3and 3typesetting 3industry.';
-var recorrerPalabras = function (text, msPerWord, callback) {
-    if (msPerWord === void 0) { msPerWord = 1000; }
-    var splitText = text.split(' ');
-    var counter = 0;
-    var myInterval = setInterval(function () {
-        if (counter === splitText.length) {
-            clearInterval(myInterval);
-            console.log(counter + " palabras encontradas!");
-            callback();
-        }
-        else {
-            console.log(splitText[counter]);
-            counter++;
-        }
-    }, msPerWord);
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) {return value instanceof P ? value : new P(function (resolve) {resolve(value);});}
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) {try {step(generator.next(value));} catch (e) {reject(e);} }
+        function rejected(value) {try {step(generator["throw"](value));} catch (e) {reject(e);} }
+        function step(result) {result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);}
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
-var myCallback = function () {
+const text1 = '1Lorem 1Ipsum 1is 1simply';
+const text2 = '2dummy 2text 2of 2the 2printing';
+const text3 = '3and 3typesetting 3industry.';
+const recorrerPalabras = (text, msPerWord = 1000, callback) => __awaiter(void 0, void 0, void 0, function* () {
+    const splitText = text.split(' ');
+    for (let i = 0; i < splitText.length; i++) {
+        yield new Promise(resolve => setTimeout(resolve, msPerWord));
+        console.log(splitText[i]);
+    }
+    console.log(`${splitText.length} palabras encontradas`);
+    callback();
+});
+const myCallback = () => {
     console.log('proceso completo');
 };
-recorrerPalabras(text1, 800, myCallback);
-recorrerPalabras(text2, 800, myCallback);
-recorrerPalabras(text3, 800, myCallback);
+const awaitFun = () => __awaiter(void 0, void 0, void 0, function* () {
+    yield recorrerPalabras(text1, 1800, myCallback);
+    yield recorrerPalabras(text2, 1800, myCallback);
+    yield recorrerPalabras(text3, 1800, myCallback);
+});
+awaitFun();
--- challenges/3-asynchronous-javascript/index.ts
+++ challenges/3-asynchronous-javascript/index.ts
@@ -2,21 +2,16 @@ const text1: string = '1Lorem 1Ipsum 1is 1simply';
 const text2: string = '2dummy 2text 2of 2the 2printing';
 const text3: string = '3and 3typesetting 3industry.';
 
-const recorrerPalabras: Function = (text: string, msPerWord: number = 1000, callback: Function): void => {
+const recorrerPalabras: Function = async (text: string, msPerWord: number = 1000, callback: Function) => {
 
   const splitText: string[] = text.split(' ');
-  let counter: number = 0;
 
-  const myInterval: number = setInterval((): void => {
-    if (counter === splitText.length) {
-      clearInterval(myInterval);
-      console.log(`${counter} palabras encontradas!`);
-      callback();
-    } else {
-      console.log(splitText[counter]);
-      counter++;
-    }
-  }, msPerWord)
+  for (let i: number = 0; i < splitText.length; i++) {
+    await new Promise(resolve => setTimeout(resolve, msPerWord));
+    console.log(splitText[i]);
+  }
+  console.log(`${splitText.length} palabras encontradas`);
+  callback();
 
 }
 
@@ -24,6 +19,9 @@ const myCallback: Function = (): void => {
   console.log('proceso completo');
 }
 
-recorrerPalabras(text1, 800, myCallback,);
-recorrerPalabras(text2, 800, myCallback,);
-recorrerPalabras(text3, 800, myCallback,);
+const awaitFun = async () => {
+  await recorrerPalabras(text1, 1800, myCallback,);
+  await recorrerPalabras(text2, 1800, myCallback,);
+  await recorrerPalabras(text3, 1800, myCallback,);
+}
+awaitFun();
