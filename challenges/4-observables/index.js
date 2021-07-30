7c8be2b3816ed5227772f8aa80c63a2591bf6bc8
Enzo Arguello <xlsones003@gmail.com>
2021-07-30 17:16:47 -0300

    updated challenge 4

--- challenges/4-observables/index.js
+++ challenges/4-observables/index.js
@@ -1,13 +1,48 @@
-import {Observable, fromEvent} from "rxjs";
+const {Observable, fromEvent} = rxjs;
+const {filter, map} = rxjs.operators;
 
 document.addEventListener('DOMContentLoaded', () => {
+
+  function blockInput(reason, subs, input, mirror, raiseError) {
+    subs.unsubscribe();
+    input.setAttribute('disabled', '');
+    mirror.textContent = '';
+    console.log(reason);
+    if (raiseError) throw new Error(reason);
+  }
+
+
+
   const inputText = document.getElementById('ejemplosInput');
+  const espejo = document.getElementById('espejo');
 
-  const observable = fromEvent(inputText, 'keyup');
+  const observable = fromEvent(inputText, 'keyup').pipe(map(e => e.target.value.split('').reverse().join('')));
 
-  observable.subscribe((e) => {
-    console.log('keyup', e.target.value);
-    inputText.textContent = "asd";
+  const subscription = observable.subscribe((e) => {
+
+    //usando metodos nativos 
+    //e = e.target.value.split('').reverse().join('')
+
+    espejo.textContent = e;
+    //error
+    if (/rorre/.test(e)) {
+      blockInput('Salgo por error', subscription, inputText, espejo, true);
+    }
+    //complete
+    else if (/etelpmoc/.test(e)) {
+      blockInput('Complete', subscription, inputText, espejo, false);
+    }
+
   });
+
+  setTimeout(() => {
+    subscription.unsubscribe();
+    blockInput('Desubscripcion automatica', subscription, inputText, espejo, false);
+  }, 30000);
+
+
+
+
+
 });
 
