d71866eacbd8f49e719366013bc2ed5af4aad5c1
Enzo Arguello <xlsones003@gmail.com>
2021-07-31 16:05:26 -0300

    added challenge 5

---
+++ challenges/5-server-in-nodejs/index.js
@@ -0,0 +1,27 @@
+
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+const http = require('http');
+
+http.createServer((request, response) => {
+
+  const item = {
+    id: randomInteger(1, 10),
+    title: `Producto ${randomInteger(1, 10)}`,
+    price: randomNumber(0.00, 9999).toFixed(2),
+    thumbnail: `Foto ${randomInteger(1, 10)}`,
+  }
+
+  response.end(JSON.stringify(item));
+
+})
+  .listen(3001, function () {
+    console.log(`Servidor escuchando en http://localhost:${this.address().port}`);
+  });
+
