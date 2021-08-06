9d7f36a6199021fa0adc439c14774a3ce16df6ff
Enzo Arguello <xlsones003@gmail.com>
2021-08-06 18:45:39 -0300

    updated challenge 3 v2

--- challenges/3-asynchronous-javascript/index.js
+++ challenges/3-asynchronous-javascript/index.js
@@ -1,31 +1,22 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) {return value instanceof P ? value : new P(function (resolve) {resolve(value);});}
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) {try {step(generator.next(value));} catch (e) {reject(e);} }
-        function rejected(value) {try {step(generator["throw"](value));} catch (e) {reject(e);} }
-        function step(result) {result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);}
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 const text1 = '1Lorem 1Ipsum 1is 1simply';
 const text2 = '2dummy 2text 2of 2the 2printing';
 const text3 = '3and 3typesetting 3industry.';
-const recorrerPalabras = (text, msPerWord = 1000, callback) => __awaiter(void 0, void 0, void 0, function* () {
+const recorrerPalabras = async (text, callback, msPerWord = 1000) => {
     const splitText = text.split(' ');
     for (let i = 0; i < splitText.length; i++) {
-        yield new Promise(resolve => setTimeout(resolve, msPerWord));
+        await new Promise(resolve => setTimeout(resolve, msPerWord));
         console.log(splitText[i]);
     }
     console.log(`${splitText.length} palabras encontradas`);
-    callback();
-});
-const myCallback = () => {
-    console.log('proceso completo');
+    callback(splitText.length);
 };
-const awaitFun = () => __awaiter(void 0, void 0, void 0, function* () {
-    yield recorrerPalabras(text1, 1800, myCallback);
-    yield recorrerPalabras(text2, 1800, myCallback);
-    yield recorrerPalabras(text3, 1800, myCallback);
-});
-awaitFun();
+recorrerPalabras(text1, (res1) => {
+    recorrerPalabras(text2, (res2) => {
+        recorrerPalabras(text3, (res3) => {
+            const amountOfWords = res1 + res2 + res3;
+            console.log("Proceso completo");
+            console.log(`Cantidad de palabras totales ${amountOfWords}`);
+        }, 2000);
+    }, 2000);
+}, 2000);
--- challenges/3-asynchronous-javascript/index.ts
+++ challenges/3-asynchronous-javascript/index.ts
@@ -2,7 +2,7 @@ const text1: string = '1Lorem 1Ipsum 1is 1simply';
 const text2: string = '2dummy 2text 2of 2the 2printing';
 const text3: string = '3and 3typesetting 3industry.';
 
-const recorrerPalabras: Function = async (text: string, msPerWord: number = 1000, callback: Function) => {
+const recorrerPalabras: Function = async (text: string, callback: Function, msPerWord: number = 1000) => {
 
   const splitText: string[] = text.split(' ');
 
@@ -11,17 +11,16 @@ const recorrerPalabras: Function = async (text: string, msPerWord: number = 1000
     console.log(splitText[i]);
   }
   console.log(`${splitText.length} palabras encontradas`);
-  callback();
+  callback(splitText.length);
 
 }
 
-const myCallback: Function = (): void => {
-  console.log('proceso completo');
-}
-
-const awaitFun = async () => {
-  await recorrerPalabras(text1, 1800, myCallback,);
-  await recorrerPalabras(text2, 1800, myCallback,);
-  await recorrerPalabras(text3, 1800, myCallback,);
-}
-awaitFun();
+recorrerPalabras(text1, (res1: number) => {
+  recorrerPalabras(text2, (res2: number) => {
+    recorrerPalabras(text3, (res3: number) => {
+      const amountOfWords = res1 + res2 + res3;
+      console.log("Proceso completo");
+      console.log(`Cantidad de palabras totales ${amountOfWords}`)
+    }, 2000)
+  }, 2000)
+}, 2000)
