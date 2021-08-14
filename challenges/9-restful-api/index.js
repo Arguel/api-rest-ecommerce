c2a165f8a5b30dbfc24f81ce1f7008c295c3c50f
Enzo Arguello <xlsones003@gmail.com>
2021-08-14 18:54:02 -0300

    added challenge 9

---
+++ challenges/9-restful-api/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const routes = require('./routes');
+const app = express();
+const port = 3000
+
+//Middlewares
+app.use(express.text());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//Routes
+app.use('/api', routes);
+app.get('', (req, res,) => {
+  res.sendFile('./index.html', {root: __dirname});
+})
+
+//Port
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
---
+++ challenges/9-restful-api/routes.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const router = express.Router();
+
+let products = [
+  {
+    "title": "water",
+    "price": "200",
+    "thumbnail": "./img/water.png",
+    "id": 0
+  },
+  {
+    "title": "orange",
+    "price": "10",
+    "thumbnail": "./img/orange.png",
+    "id": 1
+  },
+  {
+    "title": "milk",
+    "price": "90",
+    "thumbnail": "./img/milk.png",
+    "id": 2
+  }
+];
+
+router.get('/products/list', (req, res,) => {
+  if (products.length) {
+    res.json(products);
+  } else {
+    res.json({
+      "error": "No products loaded"
+    })
+  }
+});
+
+router.get('/products/list/:id', (req, res,) => {
+  const productId = req.params.id;
+  if (productId >= 0 && productId <= products.length - 1) {
+    res.json(products[productId]);
+  } else {
+    res.send('Product not found');
+  }
+});
+
+router.post('/products/save', (req, res,) => {
+  const data = req.body;
+  products = [...products, {...data, "id": products.length}];
+  res.json(products);
+});
+
+router.put('/products/update/:id', (req, res,) => {
+  const productId = req.params.id;
+  if (productId >= 0 && productId <= products.length - 1) {
+    products[productId] = {...req.body, "id": productId};
+    res.json(products[productId]);
+  } else {
+    res.send('Product not found');
+  }
+});
+
+router.delete('/products/delete/:id', (req, res,) => {
+  const productId = req.params.id;
+  if (productId >= 0 && productId <= products.length - 1) {
+    const deletedItem = products.splice(productId, 1);
+    res.json(deletedItem[0]);
+  } else {
+    res.send('Product not found');
+  }
+});
+
+module.exports = router;
