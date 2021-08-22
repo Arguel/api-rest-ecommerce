67bbbb533a0d1db46d83b445826f1b3da5423d32
Enzo Arguello <xlsones003@gmail.com>
2021-08-22 09:50:59 -0300

    added husky and eslint to the project, also added the template for challenge 11

---
+++ challenges/11-pug-and-ejs/index.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const handlebars = require("express-handlebars");
+const port = 3000;
+
+let rawData = fs.readFileSync("products.json", "utf-8");
+let products = JSON.parse(rawData.toString("utf-8"));
+
+//Middlewares
+app.use(express.text());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Handlebars
+app.engine(
+  "hbs",
+  handlebars({
+    extname: ".hbs",
+    defaultLayout: "main.hbs",
+    layoutsDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + "/views/partials",
+  })
+);
+
+//Engines
+app.set("view engine", "hbs");
+app.set("views", "./views");
+
+//Static files
+app.use(express.static("public"));
+
+//Routes
+app.get("/", (req, res) => {
+  res.render("index", { products: [...products] });
+});
+
+app.get("/api/products", (req, res) => {
+  if (products.length > 1) {
+    res.render("products", { products: [...products] });
+  } else {
+    res.send("There are no products.");
+  }
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const productId = req.params.id;
+  if (productId >= 0 && productId <= products.length - 1) {
+    res.render("products", { products: [products[productId]] });
+  } else {
+    res.send("Product not found");
+  }
+});
+
+app.post("/api/products", (req, res) => {
+  const data = req.body;
+  products = [...products, { ...data, id: products.length }];
+  res.render("products", { products: [...products] });
+});
+
+app.put("/api/products/:id", (req, res) => {
+  const productId = req.params.id;
+  if (productId >= 0 && productId <= products.length - 1) {
+    products[productId] = { ...req.body, id: productId };
+    res.render("products", { products: [products[productId]] });
+  } else {
+    res.send("Product not found");
+  }
+});
+
+app.delete("/api/products/:id", (req, res) => {
+  const productId = req.params.id;
+  if (productId >= 0 && productId <= products.length - 1) {
+    const deletedItem = products.splice(productId, 1);
+    res.render("products", { products: [deletedItem[0]] });
+  } else {
+    res.send("Product not found");
+  }
+});
+
+//Port
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
