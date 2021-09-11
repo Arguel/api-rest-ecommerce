37dfcf61302fc8cb17c40c56a01c5fbdf4662744
Enzo Arguello <xlsones003@gmail.com>
2021-09-11 13:53:52 -0300

    added files and modules of the final project part 1

---
+++ challenges/17-sqlite3/dist/middlewares/auth.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.isAdmin = exports.userProperties = void 0;
+function userProperties(req, res, next) {
+    req.body.user = {
+        login: true,
+        isAdmin: true,
+    };
+    next();
+}
+exports.userProperties = userProperties;
+function isAdmin(req, res, next) {
+    var user = req.body.user;
+    if (user.isAdmin)
+        next();
+    else
+        res.status(401).json({
+            error: 401,
+            description: "Route '" + req.originalUrl + "' - Method '" + req.method + "' unauthorized",
+        });
+}
+exports.isAdmin = isAdmin;
---
+++ challenges/17-sqlite3/src/server.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import morgan from "morgan";
+import path from "path";
+import productsRoutes from "./routes/products.routes";
+import cartRoutes from "./routes/cart.routes";
+import "./database";
+import {userProperties} from "./middlewares/auth";
+const app: express.Application = express();
+
+// Settings
+app.set("port", process.env.PORT || 8080);
+
+// Middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+
+// Routes
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+app.use("/products", userProperties, productsRoutes);
+app.use("/cart", cartRoutes);
+app.get("*", (req: express.Request, res: express.Response) => {
+  res.status(404).json({
+    error: 404,
+    description: `Route '${req.originalUrl}' - Method '${req.method}' not found`,
+  });
+});
+
+// Static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Starting the server
+app.listen(app.get("port"), () => {
+  console.log(`Example app listening at http://localhost:${app.get("port")}`);
+});
