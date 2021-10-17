82a37d382ed8407826c9cc202f482956469cf534
Enzo Arguello <xlsones003@gmail.com>
2021-10-17 15:40:39 -0300

    further restructuring of folders, settings were modified to adjust to this new folder scheme (part 1)

--- challenges/25-login-with-facebook/server/src/routes/views.routes.ts
+++ challenges/25-login-with-facebook/server/src/routes/views.routes.ts
@@ -2,7 +2,7 @@ import {Router, IRouter} from "express";
 import {auth} from "../services/auth/auth";
 import {ViewsController} from "../controller/views.controllers";
 import passport from "passport";
-import "../services/auth/strategies/passport.local";
+import "../services/auth/strategies/passport.facebook";
 
 const controller: ViewsController = new ViewsController();
 
@@ -10,21 +10,19 @@ const router: IRouter = Router();
 
 router.get("/login", controller.getLogin.bind(controller));
 
-router.post(
-  "/login",
-  passport.authenticate("login", {failureRedirect: "/faillogin"}),
-  controller.postLogin.bind(controller),
+router.get("/auth/facebook", passport.authenticate("facebook"));
+
+router.get(
+  "/auth/facebook/callback",
+  passport.authenticate("facebook", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  }),
 );
 
 router.get("/faillogin", controller.getFailLogin.bind(controller));
 
 router.get("/register", controller.getRegister.bind(controller));
-
-router.post(
-  "/register",
-  passport.authenticate("register", {failureRedirect: "/failregister"}),
-  controller.postRegister.bind(controller),
-);
 
 router.get("/failregister", controller.getFailRegister.bind(controller));
 
---
+++ challenges/25-login-with-facebook/server/src/services/auth/strategies/passport.facebook.ts
@@ -0,0 +1,34 @@
+import passport from "passport";
+import {Strategy as FacebookStrategy} from "passport-facebook";
+import {UserModel} from "../../../models/mongodb/user";
+import {IUser} from "../../../utils/modelsInterfaces";
+import dotenv from "dotenv";
+
+// Environment Variables
+dotenv.config();
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_APP_ID as string,
+      clientSecret: process.env.FACEBOOK_APP_SECRET as string,
+      callbackURL: "asd",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      UserModel.findOrCreate(profile.id, function (err: Error, user: IUser) {
+        if (err) return done(err);
+        done(null, user);
+      });
+    },
+  ),
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, (user as IUser)._id);
+});
+
+passport.deserializeUser(function (id, done) {
+  UserModel.findById(id, function (err: Error, user: IUser) {
+    done(err, user);
+  });
+});
