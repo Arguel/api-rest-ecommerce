66b6b654ba12339af3b5bd36fd682a944096a353
Enzo Arguello <xlsones003@gmail.com>
2021-10-27 16:32:59 -0300

    added challenge base 27

---
+++ challenges/27-run-node-servers/server/dist/controller/products.controllers.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ProductsController = void 0;
+var factoryType_1 = require("../config/database/factoryType");
+var products_1 = require("./factory/products");
+// Main switch/selector for persistence types
+var factory = new products_1.FactoryProducts();
+var ProductsController = /** @class */ (function () {
+    function ProductsController() {
+        /* Depending on the type of persistence selected in "/config/factoryType.ts" we will use
+         * a particular database model or another */
+        this.factory = factory.type(factoryType_1.persistenceType);
+    }
+    ProductsController.prototype.getProducts = function (req, res) {
+        return this.factory.getProducts(req, res);
+    };
+    ProductsController.prototype.getProductById = function (req, res) {
+        return this.factory.getProductById(req, res);
+    };
+    ProductsController.prototype.addProduct = function (req, res) {
+        return this.factory.addProduct(req, res);
+    };
+    ProductsController.prototype.updateProductById = function (req, res) {
+        return this.factory.updateProductById(req, res);
+    };
+    ProductsController.prototype.deleteProductById = function (req, res) {
+        return this.factory.deleteProductById(req, res);
+    };
+    ProductsController.prototype.filter = function (req, res) {
+        return this.factory.filter(req, res);
+    };
+    return ProductsController;
+}());
+exports.ProductsController = ProductsController;
---
+++ challenges/27-run-node-servers/server/src/controller/factory/cart.ts
@@ -0,0 +1,17 @@
+import {MongodbCart} from "../mongodb/cart";
+import {MysqlCart} from "../mysql/cart";
+import {ICrudCart} from "../../libs/interfaces/crud.interfaces";
+
+export class FactoryCart {
+  // The type() method returns our DAO (data access object)
+  type(type: string): ICrudCart {
+    switch (type) {
+      case "mongodb":
+        return new MongodbCart();
+      case "mysql":
+        return new MysqlCart();
+      default:
+        return new MongodbCart();
+    }
+  }
+}
