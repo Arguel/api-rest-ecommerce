312a959f22e686c8c4b2fc01ac488525b0e2d98e
Enzo Argüello <arguelar@gmail.com>
2022-06-27 16:54:06 -0300

    Updated products module

--- challenges/3e-server-with-express/productsNew.js
+++ challenges/3e-server-with-express/productsNew.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+import {writeFile, readFile} from "fs.promises";
 
 export class FsHandler {
   constructor(fileName) {
@@ -6,51 +6,38 @@ export class FsHandler {
   }
 
   save = async (obj) => {
-    const data = await this.getAll();
-    // We assign the id of the last object of the array or zero in case of being the first item
-    this.currentId = data.length > 0 ? data[data.length - 1].id : 0;
-    obj.id = ++this.currentId;
-    this.products = [...data, obj];
+    const data = await this.readData().catch(() => {
+      console.warn("Error reading the file, creating a new one...");
+      return [];
+    });
 
-    await fs.promises
-      .writeFile(this.fileName, JSON.stringify(this.products, null, "\t"))
-      .catch((err) => console.error("File write error:", err));
+    // We assign the id of the last object of the array or zero in case of being the first item
+    let currentId = data.length > 0 ? data[data.length - 1].id : 0;
+    obj.id = ++currentId;
+    data = [...data, obj];
+
+    await this.writeData(data);
+
     return obj.id;
   };
 
   getById = async (id) => {
-    const data = await this.getAll();
+    const data = await this.readData();
     const obj = data.find((obj) => obj.id === id);
     return obj ? obj : null;
   };
 
-  getAll = async () => {
-    try {
-      return JSON.parse(await fs.promises.readFile(this.fileName, "utf-8"));
-    } catch (error) {
-      console.warn("File reading error, returning a new one...", error);
-      return [];
-    }
-  };
+  readData = () =>
+    readFile(this.fileName, "utf-8").then((data) => JSON.parse(data));
+
+  writeData = (data) =>
+    writeFile(this.fileName, JSON.stringify(data, null, "\t"));
 
   deleteById = async (id) => {
-    try {
-      this.products = this.products.filter((obj) => obj.id !== id);
-      await fs.promises.writeFile(
-        this.fileName,
-        JSON.stringify(this.products, null, "\t"),
-      );
-    } catch (error) {
-      console.log("Error when it was deleted:", error);
-    }
+    let data = await this.readData();
+    data = data.filter((obj) => obj.id !== id);
+    this.writeData(data);
   };
 
-  deleteAll = async () => {
-    try {
-      this.products = [];
-      await fs.promises.writeFile(this.fileName, "[]");
-    } catch (error) {
-      console.log("File write error:", error);
-    }
-  };
+  deleteAll = () => this.writeData([]);
 }
