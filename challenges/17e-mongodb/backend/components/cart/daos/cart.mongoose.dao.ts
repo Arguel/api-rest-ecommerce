df1a342180da6bd2bc6d0fb78425007ac429dc4d
Enzo Arguello <arguelar@gmail.com>
2022-08-24 13:30:04 -0300

    fix: added constructor, model and create method to cart dao.
    The old model was replaced and the imports were renewed.

--- challenges/17e-mongodb/backend/components/cart/daos/cart.mongoose.dao.ts
+++ challenges/17e-mongodb/backend/components/cart/daos/cart.mongoose.dao.ts
@@ -1,4 +1,4 @@
-import mongooseService from '../../../services/mongoose/mongoose.service';
+import mongoose from 'mongoose';
 import { nanoid } from 'nanoid';
 import debug from 'debug';
 import { ICreateCartDto } from '../dto/create.cart.dto';
@@ -6,12 +6,31 @@ import { IPatchCartDto } from '../dto/patch.cart.dto';
 import BaseError from '../../../common/error/base.error';
 import { ICrud } from '../../../common/types/crud.interface';
 import { BadRequestError } from '../../../common/error/bad.request.error';
+import { Cart } from '../models/cart.model';
 
 const log: debug.IDebugger = debug('app:carts-dao');
 
 export class CartsDao implements ICrud {
-  public async create() {
-    return ``;
+  constructor() {
+    log('Created new instance of CartsDao');
+  }
+
+  public async create(cartFields: ICreateCartDto) {
+    try {
+      const cartId: string = nanoid();
+      const cart = new Cart({
+        ...cartFields,
+        _id: cartId,
+      });
+      await cart.save();
+      return cart.id;
+    } catch (err) {
+      if (err instanceof mongoose.Error.ValidationError) {
+        const message = Object.values(err.errors).map((prop) => prop.message);
+        throw new BadRequestError(message.join('. '), 'create');
+      }
+      throw new BaseError('Failed to save cart', err, 'create');
+    }
   }
 
   public async list() {
