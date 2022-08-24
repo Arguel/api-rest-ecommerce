3da7af93e5d28bbc60a2573d797181d659294a8a
Enzo Arguello <arguelar@gmail.com>
2022-08-24 07:12:13 -0300

    fix: add types and new function to get the server url.
    Added the types for typescript to avoid errors and also created the
    function to get the url of the mongo server that is running.

--- challenges/17e-mongodb/backend/services/mongoose/mongoose.service.ts
+++ challenges/17e-mongodb/backend/services/mongoose/mongoose.service.ts
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 import debug from 'debug';
 import { Global } from './types/memory.server.interface';
 import MongoMemoryServer from 'mongodb-memory-server-core';
+import config from 'config';
+import { TKeys } from '../../common/types/factory.persistence.enum';
 
 const log: debug.IDebugger = debug('app:mongoose-service');
 
@@ -15,23 +17,48 @@ class MongooseService {
     serverSelectionTimeoutMS: 5000,
     useFindAndModify: false,
   };
+  // mongoatlas
+  private atlasUser = config.get<string>('databases.mongoatlas.user');
+  private atlasPassword = config.get<string>('databases.mongoatlas.password');
+  private atlasClusterUrl = config.get<string>(
+    'databases.mongoatlas.clusterurl'
+  );
+  private atlasDatabase = config.get<string>('databases.mongoatlas.database');
+  // mongolocal
+  private localHost = config.get<string>('databases.mongolocal.host');
+  private localPort = config.get<string>('databases.mongolocal.host');
+  private localDatabase = config.get<string>('databases.mongolocal.host');
 
   constructor() {
     this.connectWithRetry();
   }
 
-  getMongoose() {
+  public getMongoose() {
     return mongoose;
   }
 
-  async getTestServer() {
-    const instance = await MongoMemoryServer.create();
-    const uri = instance.getUri();
+  public async getTestServer(): Promise<string> {
+    const instance: MongoMemoryServer = await MongoMemoryServer.create();
+    const uri: string = instance.getUri();
     global.__MONGOINSTANCE__ = instance;
     return uri.slice(0, uri.lastIndexOf('/'));
   }
 
-  connectWithRetry = async () => {
+  public getMongoUrl = async (type: TKeys): Promise<string> => {
+    if (process.env.NODE_ENV === 'test') {
+      const testServer: string = await this.getTestServer();
+      return `${testServer}/${this.atlasDatabase}`;
+    }
+
+    switch (type) {
+      case 'mongolocal':
+        return `mongodb://${this.localHost}:${this.localPort}/${this.localDatabase}`;
+      default:
+        return `mongodb+srv://${this.atlasUser}:${this.atlasPassword}@${this.atlasClusterUrl}/${this.atlasDatabase}?retryWrites=true&w=majority`;
+    }
+  };
+
+  public connectWithRetry = async (): Promise<void> => {
     try {
       log('Attempting MongoDB connection (will retry if needed)');
       await mongoose.connect(
