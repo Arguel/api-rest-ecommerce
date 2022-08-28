1009568a0a7d3814136bba3ce75cc18fd99f055b
Enzo Arguello <arguelar@gmail.com>
2022-08-28 13:54:05 -0300

    build(dist): remove dist folder and replace by build.
    BREAKING CHANGE: The distribution folder is removed, now the server will
    run in the build folder and it will be ignored in the .gitignore.

--- challenges/19e-mocks-and-normalization/backend/dist/components/cart/dto/patch.cart.dto.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicGF0Y2guY2FydC5kdG8uanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi8uLi9jb21wb25lbnRzL2NhcnQvZHRvL3BhdGNoLmNhcnQuZHRvLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==
--- challenges/19e-mocks-and-normalization/backend/dist/services/mongoose/types/memory.server.interface.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWVtb3J5LnNlcnZlci5pbnRlcmZhY2UuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi8uLi9zZXJ2aWNlcy9tb25nb29zZS90eXBlcy9tZW1vcnkuc2VydmVyLmludGVyZmFjZS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIn0=
