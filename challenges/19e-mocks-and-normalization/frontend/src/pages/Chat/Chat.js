7f05ea5d2efc4015f924cb8515ac0705ea01761e
Enzo Arguello <arguelar@gmail.com>
2022-08-30 09:31:33 -0300

    style: rename pages.

---
+++ challenges/19e-mocks-and-normalization/frontend/src/pages/Chat/Chat.js
@@ -0,0 +1,12 @@
+import ChatChannel from '../../components/ChatChannel/ChatChannel';
+
+const Chat = () => {
+  return (
+    <div className="container">
+      <h1 className="text-center mt-5 pt-4">Centro de Mensajes</h1>
+      <ChatChannel />
+    </div>
+  );
+};
+
+export default Chat;
