bdf89c12ab56f3dc6527432eb8ca01959b267955
Enzo Arguello <arguelar@gmail.com>
2022-08-31 18:48:47 -0300

    feat!: migrate to typescript.

--- challenges/19e-mocks-and-normalization/frontend/src/index.tsx
+++ challenges/19e-mocks-and-normalization/frontend/src/index.tsx
@@ -8,7 +8,9 @@ import App from './App';
 import { store } from './store';
 import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 
 root.render(
   <React.StrictMode>
