8505ed1e76a9669536feb270ad849273ba590d14
Enzo Arguello <arguelar@gmail.com>
2022-09-04 17:36:24 -0300

    fix: add comments and routes to rrd v6

--- challenges/19e-mocks-and-normalization/frontend/src/App.tsx
+++ challenges/19e-mocks-and-normalization/frontend/src/App.tsx
@@ -1,19 +1,16 @@
 import './App.css';
 
-import {
-  HashRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 // Components
+import Layout from 'components/Layout/Layout';
+// import RequireAuth "components/RequireAuth/RequireAuth"
 
-// Screens
+// Pages
 // import ProductScreen from './screens/ProductScreen';
 // import CartScreen from './screens/CartScreen';
-import Login from './pages/Login/Login';
-import Signup from './pages/Signup/Signup';
+import Login from 'pages/Login/Login';
+import Signup from 'pages/Signup/Signup';
 import Chat from './pages/Chat/Chat';
 // import Cart from './pages/Cart/Cart';
 // import SuccesfulOrder from './pages/SuccesfulOrder/SuccesfulOrder';
@@ -22,35 +19,45 @@ import Chat from './pages/Chat/Chat';
 // import Products from './pages/Products/Products';
 import Shop from './pages/Shop/Shop';
 
-const isLoggedIn = true;
-
 function App() {
   return (
-    <Router>
-      <main className="app">
-        <Switch>
-          <Route path="/login" component={Login} />
-          <Route path="/signup" component={Signup} />
-          <Route path="/chat">
-            {isLoggedIn ? <Chat /> : <Redirect to="/login" />}
-          </Route>
-          {/* <Route path="/cart"> */}
-          {/*   {isLoggedIn ? <Cart /> : <Redirect to="/login" />} */}
-          {/* </Route> */}
-          {/* <Route path="/successful-order"> */}
-          {/*   {isLoggedIn ? <SuccesfulOrder /> : <Redirect to="/login" />} */}
-          {/* </Route> */}
-          {/* <Route path="/orders"> */}
-          {/*   {isLoggedIn ? <Orders /> : <Redirect to="/login" />} */}
-          {/* </Route> */}
-          {/* <Route path="/account"> */}
-          {/*   {isLoggedIn ? <Account /> : <Redirect to="/login" />} */}
-          {/* </Route> */}
-          {/* <Route path="/products/:id" component={Products} /> */}
-          <Route path="/" component={Shop} />
-        </Switch>
-      </main>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* public routes */}
+        <Route index element={<Shop />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+
+        {/* protected routes */}
+        {/*<Route element={<RequireAuth />}>*/}
+        <Route path="chat" element={<Chat />} />
+        {/*   <Route path="userslist" element={<UsersList />} /> */}
+        {/*</Route>*/}
+      </Route>
+      {/* <main className="app"> */}
+      {/*   <Switch> */}
+      {/*     <Route path="/login" component={Login} /> */}
+      {/*     <Route path="/signup" component={Signup} /> */}
+      {/*     <Route path="/chat"> */}
+      {/*       {isLoggedIn ? <Chat /> : <Redirect to="/login" />} */}
+      {/*     </Route> */}
+      {/*     {/1* <Route path="/cart"> *1/} */}
+      {/*     {/1*   {isLoggedIn ? <Cart /> : <Redirect to="/login" />} *1/} */}
+      {/*     {/1* </Route> *1/} */}
+      {/*     {/1* <Route path="/successful-order"> *1/} */}
+      {/*     {/1*   {isLoggedIn ? <SuccesfulOrder /> : <Redirect to="/login" />} *1/} */}
+      {/*     {/1* </Route> *1/} */}
+      {/*     {/1* <Route path="/orders"> *1/} */}
+      {/*     {/1*   {isLoggedIn ? <Orders /> : <Redirect to="/login" />} *1/} */}
+      {/*     {/1* </Route> *1/} */}
+      {/*     {/1* <Route path="/account"> *1/} */}
+      {/*     {/1*   {isLoggedIn ? <Account /> : <Redirect to="/login" />} *1/} */}
+      {/*     {/1* </Route> *1/} */}
+      {/*     {/1* <Route path="/products/:id" component={Products} /> *1/} */}
+      {/*     <Route path="/" component={Shop} /> */}
+      {/*   </Switch> */}
+      {/* </main> */}
+    </Routes>
   );
 }
 
