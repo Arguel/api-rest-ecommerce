32719dd076027a096905bc2ed18857e771b5dec9
Enzo Arguello <arguelar@gmail.com>
2022-09-07 05:26:07 -0300

    feat: add footer component.

--- challenges/19e-mocks-and-normalization/frontend/src/pages/Shop/Shop.tsx
+++ challenges/19e-mocks-and-normalization/frontend/src/pages/Shop/Shop.tsx
@@ -2,6 +2,7 @@ import Navbar from '../../components/Navbar/Navbar';
 import React from 'react';
 import Banner from 'components/Banner/Banner';
 import Items from 'components/Items/Items';
+import Footer from 'components/Footer/Footer';
 
 const Shop = () => {
   return (
@@ -17,6 +18,7 @@ const Shop = () => {
         </section>
         {/*shop*/}
       </main>
+      <Footer />
     </React.Fragment>
   );
 };
